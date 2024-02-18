fe7c41cbe102de57d1f2095874c08615a03adaa5
vinnu-badlani <you@example.com>
2024-02-18 15:54:32 +0530

    Committing changes to frontend/src/App.js before pulling updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import CreateProfile from './pages/createprofile';
 import PostCreationForm from './pages/post_form';
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
+import ProfilePage from './pages/profile';
+
 
 // Create a dark theme
 const darkTheme = createTheme({
@@ -24,6 +28,9 @@ export default function App() {
           <Route path="/pages/dashboard" element={<Dashboard />} />
           <Route path="/pages/createprofile" element={<CreateProfile />} />
           <Route path='/pages/post_form' element={<PostCreationForm />}/>
+          <Route path='/pages/login' element={<LoginPage />}/>
+          <Route path='/pages/register' element={<RegisterPage />}/>
+          <Route path='/pages/profile' element={<ProfilePage />}/>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
